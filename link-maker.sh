1885dd2f50c844cc020b34166d66438f7d261dd6
plzcookee <plzcookee@gmail.com>
2025-08-07 20:01:55 -0400

    run

--- link-maker.sh
+++ link-maker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 wget --tries=0 --wait=5 --random-wait --input-file=links.txt
-```
 for i in {1..392}; do
 	echo "https://app.thahub.lol/_next/static/chunks/6434-f1ba83ef6f4d8ff1.js"
 done
